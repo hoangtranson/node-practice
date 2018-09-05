b02ce05c511aac3eb731240da408f6751c008ed8
Hoang Tran <hayden.tran.d@gmail.com>
2018-09-05 17:42:18 +0700

    git add public file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const chalk = require('chalk');
 const app = express();
+
+const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 
-app.use(morgan('tiny'))
+const path = require('path');
+
+function getView(file){
+    return path.join(__dirname, '/views/', `${file}.html`);
+}
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/views/index.html')
+    res.sendfile(getView('index'))
 })
 
 
