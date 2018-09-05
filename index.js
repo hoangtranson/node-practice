fd3e2a9b2ab18c7e7b65fcb83a710da1cd12ae5f
Hoang Tran <hayden.tran.d@gmail.com>
2018-09-05 11:48:50 +0700

    init node project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('node 1 project')
+})
+
+
+app.listen(3000, () => {
+    console.log('listen on port 3000')
+})
