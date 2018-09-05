8d129376d812cd2f5ea861def0cd111fc8f00597
Hoang Tran <hayden.tran.d@gmail.com>
2018-09-05 17:19:53 +0700

    add views index.html

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
+const chalk = require('chalk');
 const app = express();
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
+app.use(morgan('tiny'))
 app.get('/', (req, res) => {
     res.send('node 1 project')
 })
 
 
 app.listen(3000, () => {
-    console.log('listen on port 3000')
+    debug(`listen on port ${chalk.green('3000')}`)
 })
