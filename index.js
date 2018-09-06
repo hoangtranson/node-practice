8958ac5074d4c9e0d10822f1dd5b74232b9e62d4
Hoang Tran <hayden.tran.d@gmail.com>
2018-09-06 10:02:54 +0700

    add template

--- index.js
+++ index.js
@@ -15,15 +15,30 @@ app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     const data = {
         title: 'my custom title',
         list: ['a', 'b']
     }
     res.render('index', data);
-})
+});
 
+app.get('/landing', (req, res) => {
+    res.render('landing-page');
+});
 
+app.get('/login', (req, res) => {
+    res.render('login-page');
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile-page');
+});
+
+app.get('/template', (req, res) => {
+    res.render('template');
+});
 app.listen(3000, () => {
     debug(`listen on port ${chalk.green('3000')}`)
-})
+});
